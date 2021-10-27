35e4a9b5e78aef2253f551c46db03f3fc28e405b
Lihi <lihise9@gmail.com>
2021-10-27 11:46:09 +0300

    Added gallery

--- js/gallery-controller.js
+++ js/gallery-controller.js
@@ -1,2 +1,21 @@
 'use strict';
-console.log('GALLERY CONTROLLER 🍇');
+
+function onInit() {
+    console.log('GALLERY CONTROLLER 🍇');
+
+    renderGallery();
+}
+
+function renderGallery() {
+    console.log('Rendering imgs...');
+
+    var imgs = getImgs();
+    console.log(imgs);
+
+    var strHtmls = imgs.map((img) => {
+        return `<img src="img/${img.id}.jpg">`;
+    });
+
+    var elContainer = document.querySelector('.gallery-container');
+    elContainer.innerHTML = strHtmls.join('');
+}
--- js/service/gallery-service.js
+++ js/service/gallery-service.js
@@ -1,2 +1,23 @@
 'use strict';
 console.log('Gallery service');
+
+// var gKeywords = {'happy': 12,'funny puk': 1}
+var gImgs = [];
+// {id: 1, url: `img/${id}.jpg`, keywords: ['happy']}
+
+createImgs();
+
+function createImg(id, keywords = 'happy') {
+    var img = { id, url: `img/${id}.jpg`, keywords: ['happy'] };
+    gImgs.push(img);
+}
+
+function createImgs() {
+    for (var i = 1; i <= 18; i++) {
+        createImg(i);
+    }
+}
+
+function getImgs() {
+    return gImgs;
+}
